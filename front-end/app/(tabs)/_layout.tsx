42858ae0b9a7ae1bacb9c4b00c570834f2fcb7a5
Lee Napthine <lee.napthine@gmail.com>
2025-10-18 09:36:23 -0700

    Create two new tabs in layout view for camera and gallery.\n - These may be temoporary and serve as a proof of concept for the camera and photo features

--- front-end/app/(tabs)/_layout.tsx
+++ front-end/app/(tabs)/_layout.tsx
@@ -24,6 +24,20 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="camera"
+        options={{
+          title: 'Camera',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="camera.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="gallery"
+        options={{
+          title: 'Gallery',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="photo.on.rectangle" color={color} />,
+        }}
+      />
+      <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
