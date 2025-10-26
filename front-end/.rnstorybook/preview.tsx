7391fbe4177d4eab4124db433ebe0cc9c2d3d50a
tdgao <mr.trumgao@gmail.com>
2025-10-26 14:15:00 -0700

    add storybook decorator to make background work

--- front-end/.rnstorybook/preview.tsx
+++ front-end/.rnstorybook/preview.tsx
@@ -1,21 +1,14 @@
-import type { Preview } from '@storybook/react-native';
+import { type Preview } from '@storybook/react-native';
+import { View } from 'react-native';
 
 const preview: Preview = {
-  parameters: {
-    backgrounds: {
-      options: {
-        // 👇 Default options
-        dark: { name: 'Dark', value: '#424245' },
-        light: { name: 'Light', value: '#F7F9F2' },
-      },
-    },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
+  decorators: [
+    (Story, c) => (
+      <View style={{ padding: 24, flex: 1, backgroundColor: '#424245' }}>
+        <Story />
+      </View>
+    ),
+  ],
 };
 
 export default preview;
