d31884097d7a36b4e79024071e6a43e02d14b98b
tdgao <mr.trumgao@gmail.com>
2025-10-29 20:44:52 -0700

    small fix

--- front-end/features/photo-editor/views/photo-editor-view.tsx
+++ front-end/features/photo-editor/views/photo-editor-view.tsx
@@ -29,6 +29,7 @@ export function PhotoEditorView({
           display: 'flex',
           flexDirection: 'row',
           justifyContent: 'space-between',
+          alignItems: 'flex-start',
         }}
       >
         <Image
