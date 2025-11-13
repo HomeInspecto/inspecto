0ad14b5301fa49ce9f57186911f9d55f0a6baba2
joshbeaman <joshbeaman@shaw.ca>
2025-11-13 14:40:49 -0800

    Changes to report viewer

--- front-end/features/inspection-details/use-inspection-details.ts
+++ front-end/features/inspection-details/use-inspection-details.ts
@@ -13,8 +13,29 @@ export function useInspectionDetails(): InspectionDetailsViewProps {
 
   const onCreateReport = () => {
     if (!activeInspection) return;
+
+    const localReal_url = `http://localhost:4321/view/${activeInspection.id}`;
+    const vercelReal_url = `https://inspection-report-topaz.vercel.app/view/${activeInspection.id}`;
+
     const url = `https://inspection-report-topaz.vercel.app/view/${activeInspection.id}`;
-    Linking.openURL(url);
+    Linking.openURL(localReal_url);
+
+    // ---------------------------------------------------------------------
+    // if (!activeInspection) return;
+
+    // const propertyId = activeInspection.property?.id;
+
+    // if (!propertyId) {
+    //   console.warn("Inspection has no property.id");
+    //   return;
+    // }
+
+    // // Local Astro dev
+    // const url = `http://localhost:4321/view/${propertyId}`;
+
+    // Linking.openURL(url);
+
+    // ---------------------------------------------------------------------
   };
 
   return {
