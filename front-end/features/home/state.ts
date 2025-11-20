869401e5f1607d4218045b17f13d5f93b8903a9f
tdgao <mr.trumgao@gmail.com>
2025-11-19 21:09:37 -0800

    add inspections list loading

--- front-end/features/home/state.ts
+++ front-end/features/home/state.ts
@@ -8,16 +8,15 @@ export interface Inspection {
 }
 
 export interface InspectionStore {
-  inspections: Inspection[];
+  inspections?: Inspection[];
   createInspection: (inspection: Inspection) => void;
   setInspections: (inspections: Inspection[]) => void;
 }
 
 export const useInspectionsStore = create<InspectionStore>(set => ({
-  inspections: [],
   createInspection: (inspection: Inspection) =>
     set(state => ({
-      inspections: [...state.inspections, inspection],
+      inspections: [...(state.inspections || []), inspection],
     })),
   setInspections: (inspections: Inspection[]) =>
     set(state => ({
